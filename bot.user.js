9467e379b4a678e03f6d682caea96a7c36ad45a1
theeusstanley <dodo123bol@gmail.com>
2015-07-19 12:30:32 -0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agar.io
 // @version     3.55
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
